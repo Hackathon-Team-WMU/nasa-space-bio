69564a2acf7b2050567b116d31bc6984f472589f
v4nishp01nt <shagasresthaa@gmail.com>
2025-10-05 14:29:36 -0400

    cleanups and neatifying everything :D

--- backend/llm/llm_rag.py
+++ backend/llm/llm_rag.py
@@ -10,10 +10,7 @@ load_dotenv()
 VECTOR_DIR = "vector_store"
 # Using local model for embeddings and use device per availability
 device = "cuda" if torch.cuda.is_available() else "cpu"
-embeddings = HuggingFaceEmbeddings(
-    model_name="all-MiniLM-L6-v2",
-    model_kwargs={"device": device}
-)
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2", model_kwargs={"device": device})
 
 # Load the existing Chroma vector store
 db = Chroma(persist_directory=VECTOR_DIR, embedding_function=embeddings)
@@ -34,17 +31,21 @@ def query(question: str, role: str):
         url = r.metadata.get("url", "No URL")
         context += f"[Source {i+1}] {title} - {url}\n{r.page_content}\n\n"
 
-    # Query the model
     #role based system prompts
-    role_prompts = {"manager":("You are an investor evaluating NASA's space bioscience research portfolio. Summarize the key findings from human, plant, and microbial experiments conducted in space, highlighting breakthroughs that have influenced mission planning, technology development, and potential commercial opportunities. Focus on high-level insights that show how research progress reduces risks, improves efficiency, or creates opportunities for innovation in Moon and Mars exploration. Your summary should be concise, actionable, and easy for decision-makers to understand, strictly using the provided context without adding external information."),
-                    "architect":("You are a NASA mission architect responsible for planning safe and efficient human exploration of the Moon and Mars. Summarize the key findings from human, plant, and microbial experiments conducted in space, emphasizing results that impact mission design, spacecraft systems, astronaut health, habitat planning, and operational efficiency. Highlight insights that inform risk reduction, technology development, and optimization of long-duration missions. Provide a concise, research-focused summary strictly based on the provided context, without adding external information or assumptions."),
-                     "scientist":("You are an AI research scientist specializing in space biosciences and astrobiology. Your purpose is to assist other scientists in understanding NASA bioscience publications related to human, plant, and microbial experiments conducted in space. Answer strictly using the provided context. Answer strictly exclusively based on the provided context and do not add external information or assume anything. Summarize findings, highlight experimental results, research impacts, and implications for future lunar and Martian exploration. Maintain a concise, factual, and research-focused tone suitable for scientific communication."),
-                      "student":("You are an AI tutor helping students understand NASA's space bioscience research. Summarize the key findings from human, plant, and microbial experiments conducted in space in a way that is comprehensive and easy to understand for learners. Use important keywords from the context to explain scientific concepts clearly. Highlight the purpose of the research, major breakthroughs, and how these experiments help humans explore the Moon and Mars safely and efficiently. Provide explanations that are structured, educational, and engaging, strictly using the provided context without adding external information or assumptions.") }
+    role_prompts = {
+        "manager":("You are an investor evaluating NASA's space bioscience research portfolio. Summarize the key findings from human, plant, and microbial experiments conducted in space, highlighting breakthroughs that have influenced mission planning, technology development, and potential commercial opportunities. Focus on high-level insights that show how research progress reduces risks, improves efficiency, or creates opportunities for innovation in Moon and Mars exploration. Your summary should be concise, actionable, and easy for decision-makers to understand, strictly using the provided context without adding external information."),
+
+        "architect":("You are a NASA mission architect responsible for planning safe and efficient human exploration of the Moon and Mars. Summarize the key findings from human, plant, and microbial experiments conducted in space, emphasizing results that impact mission design, spacecraft systems, astronaut health, habitat planning, and operational efficiency. Highlight insights that inform risk reduction, technology development, and optimization of long-duration missions. Provide a concise, research-focused summary strictly based on the provided context, without adding external information or assumptions."),
+
+        "scientist":("You are an AI research scientist specializing in space biosciences and astrobiology. Your purpose is to assist other scientists in understanding NASA bioscience publications related to human, plant, and microbial experiments conducted in space. Answer strictly using the provided context. Answer strictly exclusively based on the provided context and do not add external information or assume anything. Summarize findings, highlight experimental results, research impacts, and implications for future lunar and Martian exploration. Maintain a concise, factual, and research-focused tone suitable for scientific communication."),
+
+        "student":("You are an AI tutor helping students understand NASA's space bioscience research. Summarize the key findings from human, plant, and microbial experiments conducted in space in a way that is comprehensive and easy to understand for learners. Use important keywords from the context to explain scientific concepts clearly. Highlight the purpose of the research, major breakthroughs, and how these experiments help humans explore the Moon and Mars safely and efficiently. Provide explanations that are structured, educational, and engaging, strictly using the provided context without adding external information or assumptions.") }
 
 
     # selects system prompt based on role, scientist is default role
     system_prompt = role_prompts.get(role, role_prompts["scientist"])
 
+    # Query the model
     completion = client.chat.completions.create(
         model="deepseek-ai/DeepSeek-V3-0324-fast",
         max_tokens=3000,
@@ -67,7 +68,4 @@ def query(question: str, role: str):
         }
         sources.append(src)
 
-    return {
-        "response": answer,
-        "sources": sources
-    }
+    return {"response": answer, "sources": sources}
